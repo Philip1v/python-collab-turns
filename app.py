b860cc3723320da9dea3fde8e1bc8ab5a995eb2d
lielhazzan <leal.hazzan@gmail.com>
2026-03-26 19:50:14 +0200

    Liel call func

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from utils import add_entry, read_entries
+from utils import add_entry, read_entries, delete_last_entry
 
 add_entry("A: refining update")
 
 for line in read_entries():
     print(line)
+    
+delete_last_entry()
+add_entry("B: replaced deleted entry")
